20788202a2cbd95a108a5f41252d163e8809d81e
rashokatwal <rashokatwalHK@gmail.com>
2024-12-24 20:57:49 +0530

    second commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,30 @@
+import './css/navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+const Navbar = () => {
+    return ( 
+        <div className="navbar-outer">
+            <div className="navbar-inner">
+                {/* <img src="" alt="logo" className="logo" /> */}
+                <h1 className="title">MindHop</h1>
+                <div className="searchBar-outer">
+                    <input type="text" className="searchBar" placeholder="Search for Ideas"/>
+                    <button className="searchButton">
+                        <FontAwesomeIcon icon={faMagnifyingGlass} size="lg"/>
+                    </button>
+                </div>
+                <ul className="navLinks">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/explore">Explore</a></li>
+                    <li>
+                        <button className="primary-button">
+                            Post Ideas
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+ 
+export default Navbar;
