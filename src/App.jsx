a55a78af80865911deaba202b7282a6bf39dea05
rashokatwal <rashokatwalHK@gmail.com>
2024-12-27 23:16:44 +0530

    added how it works section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Description from "./Description";
+import Steps from "./Steps";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Description />
+      <Steps />
     </div>
   )
 }
---
+++ src/Steps.jsx
@@ -0,0 +1,13 @@
+import './css/steps.css';
+
+const Steps = () => {
+    return (
+        <div className="steps-outer">
+            <div className="steps-inner">
+                <h2 className="steps-title">How It Works</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Steps;
