c3945b63d215713e263d88900e99055123e4e7bc
rashokatwal <rashokatwalHK@gmail.com>
2024-12-29 23:53:37 +0530

    styled footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 import './css/footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-solid-svg-icons';
+import { faXTwitter, faSquareFacebook, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return(
@@ -8,12 +8,17 @@ const Footer = () => {
             <div className="footer-inner">
                 <h2 className="logo">MindHop</h2>
                 <p className="footer-description">A platform for thinkers, creators, and innovators to share ideas, collaborate, and inspire change. Join us and make your vision a reality.</p>
-                <div className="links">
-                    <a href="/"><FontAwesomeIcon icon={faFacebook} /></a>
+                <div className="social-links">
                     <a href="/"><FontAwesomeIcon icon={faInstagram} /></a>
+                    <a href="/"><FontAwesomeIcon icon={faSquareFacebook} /></a>
                     <a href="/"><FontAwesomeIcon icon={faLinkedin} /></a>
-                    <a href="/"><FontAwesomeIcon icon={faTwitter} /></a>
+                    <a href="/"><FontAwesomeIcon icon={faXTwitter} /></a>
                 </div>
+                <div className="other-links">
+                    <a href="/">Privacy & Policy</a>
+                    <a href="/">FAQs</a>
+                </div>
+                
             </div>
         </div>
     )
