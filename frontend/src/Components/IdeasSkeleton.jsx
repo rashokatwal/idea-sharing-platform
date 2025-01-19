7f221761ec3a34ae0baa320e625b473508584308
rashokatwal <rashokatwalHK@gmail.com>
2025-01-19 23:47:11 +0530

    minor changes

--- frontend/src/Components/IdeasSkeleton.jsx
+++ frontend/src/Components/IdeasSkeleton.jsx
@@ -1,6 +1,5 @@
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
-import { categories, categoryColors } from '../Constants/FilterElements'
 
 const IdeasSkeleton = ({ viewType }) => {
     let properties = viewType === "grid" ? {
--- frontend/src/Pages/Idea.jsx
+++ frontend/src/Pages/Idea.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import IdeaPreview from "../Components/IdeaPreview";
-// import {ideas} from "../data/ideas";
 import {useLocation} from "react-router-dom";
 import "../Styles/idea.css";
 import axios from 'axios'; 
@@ -14,7 +13,6 @@ const Idea = () => {
         await axios
          .get(`http://localhost:3000/idea/${ideaId}`)
          .then((response) => {
-            // console.log(response.data);
             setIdeaDetails(response.data);
          })
          .catch((error) => console.log(error));
