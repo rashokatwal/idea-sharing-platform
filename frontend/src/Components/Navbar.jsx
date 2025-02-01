f21754c99aeef37f63b09f4bcdbd76314cf7ace3
rashokatwal <rashokatwalHK@gmail.com>
2025-02-01 23:42:23 +0530

    feat: enhance Navbar with user dropdown functionality and improved styles

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react';
 import '../Styles/navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
@@ -6,11 +7,25 @@ import { useAuthContext } from '../Hooks/useAuthContext';
 const Navbar = () => {
     const { signout } = useSignout();
     const userStatus = useAuthContext();
+    const [ isDropdownOpen, setIsDropdownOpen ] = useState(false);
+
+    const dropdownRef = useRef(null);
 
     const handleSignout = () => {
         signout();
         window.location.href = '/';
     }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsDropdownOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, [])
 
     return ( 
         <div className="navbar-outer">
@@ -48,18 +63,21 @@ const Navbar = () => {
                                 <FontAwesomeIcon icon="fa-regular fa-bell" />
                             </li>
                             <li style={{marginLeft: '20px', position: 'relative'}}>
-                                <img className="author-avatar" src='/src/Assets/default_user.png'/>
-                                <div className="dropdown-navbar">    
+                                <img className="author-avatar" src='/src/Assets/default_user.png' onClick={() => setIsDropdownOpen(true)}/>
+                                <div className="dropdown-navbar" ref={dropdownRef} style={{display: isDropdownOpen ? 'block' : 'none'}}>    
                                     <div className='user-details'>
                                         <img className="author-avatar" src='/src/Assets/default_user.png' style={{height: '40px', width: '40px'}}/>
                                         <div style={{marginLeft: '10px'}}>
-                                            <p style={{margin: '0', fontWeight: '600'}}>John Doe</p>
-                                            <p style={{margin: '0', fontSize: '13px'}}>@jondoe</p>
+                                            <p style={{margin: '0', fontWeight: '600'}}>Rashok Katwal</p>
+                                            <p style={{margin: '0', fontSize: '13px'}}>@rashokatwal</p>
                                         </div>
                                     </div>     
                                     <ul className="dropdown-list">
-                                        <li className="dropdown-item" onClick={handleSignout}>Sign Out</li>
-                                        <li className="dropdown-item" onClick={handleSignout}><FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" /> Sign Out</li>
+                                        <li className="dropdown-item"><FontAwesomeIcon icon="fa-regular fa-lightbulb" className='icon'/><span className='text'>My Ideas</span></li>
+                                        <li className="dropdown-item"><FontAwesomeIcon icon="fa-regular fa-bookmark" className='icon'/><span className='text'>Saved Ideas</span></li>
+                                        <li className="dropdown-item"><FontAwesomeIcon icon="fa-regular fa-bell" className='icon'/><span className='text'>Notifications</span></li>
+                                        <li className="dropdown-item"><FontAwesomeIcon icon="fa-solid fa-circle-half-stroke" className='icon'/><span className='text'>Theme</span></li>
+                                        <li className="dropdown-item" onClick={handleSignout}><FontAwesomeIcon icon="fa-solid fa-arrow-right-from-bracket" className='icon'/><span className='text'>Sign Out</span></li>
                                     </ul>
                                 </div>
                             </li>
