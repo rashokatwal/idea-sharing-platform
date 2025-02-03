2caf4860827aee78ebeeba632f81d2cce16f523c
rashokatwal <rashokatwalHK@gmail.com>
2025-02-03 19:08:24 +0530

    feat: add StepOne component to CompleteProfile page with profile setup fields and styling

--- frontend/src/Pages/CompleteProfile.jsx
+++ frontend/src/Pages/CompleteProfile.jsx
@@ -1,4 +1,5 @@
 import '../Styles/completeprofile.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CompleteProfile = () => {
     return (
@@ -6,17 +7,29 @@ const CompleteProfile = () => {
             <div className="complete-profile-inner">
                 <h1 className="header-title">Complete Your Profile</h1>
                 <p className="header-subtitle">Please complete your profile information to start using the platform.</p>
+                <StepOne />
+            </div>
+        </div>
+    )
+}
 
-                <form className="complete-profile-form">
-                    <label htmlFor="firstname">First Name</label>
-                    <input type="text" id="firstname" name="firstname" required/>
-                    <label htmlFor="lastname">Last Name</label>
-                    <input type="text" id="lastname" name="lastname" required/>
-                    <label htmlFor="dob">Date of Birth</label>
-                    <input type="text" id="dob" name="dob" required/>
-                    <label htmlFor="address">Address</label>
-                    <input type="text" id="address" name="address" required/>
-                </form>
+const StepOne = () => {
+    return (
+        <div className="step-one steps">
+            <h3 className='step-header'>
+                Step 1: Set up your profile
+            </h3>
+            <div className='step-main'>
+                <div className='upload-profile-pic'>
+                    <FontAwesomeIcon icon="fa-solid fa-plus" size='2x'color='white'/>
+                </div>
+                <p style={{margin: "30px 0px", fontWeight: '500'}}>Upload Profile Photo</p>
+                <input className='user-fullname' type='text' placeholder="John Doe"/>
+                <input className='user-username' type='text' placeholder="@johndoe"/>
+                <textarea className='user-bio' placeholder='Bio' />
+            </div>
+            <div className='next-prev-buttons'>
+                <button className='primary-button' style={{marginLeft: '-20px', fontSize: '16px'}}>Continue</button>
             </div>
         </div>
     )
